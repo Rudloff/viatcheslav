2ca181521e2efc92256732ae47b8ea2e1bae88f1
Pierre Rudloff <contact@rudloff.pro>
2017-03-20 23:25:32 +0100

    Fix Chrome download

--- scripts/linux-setup.sh
+++ scripts/linux-setup.sh
@@ -1,4 +1,4 @@
-DEBNAME=google-chrome-stable_55.0.2883.75-1_amd64.deb
+DEBNAME=google-chrome-stable_56.0.2924.87-1_amd64.deb
 DEST=widevine
 mkdir -p $DEST
 wget https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/$DEBNAME -O $DEST/$DEBNAME
