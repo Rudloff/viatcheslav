8071d04408151d1c5d50b652da7bdd731535f611
Pierre Rudloff <contact@rudloff.pro>
2018-01-05 00:34:31 +0100

    Refactor code Remove useless functions

--- main.js
+++ main.js
@@ -3,12 +3,7 @@
 var electron = require('electron'),
     widevine = require('electron-widevinecdm'),
     os = require('os'),
-    mainWindow;
-
-widevine.load(electron.app);
-
-function createWindow() {
-    var molotovAgent = {
+    molotovAgent = {
         app_id: electron.app.getName(),
         app_build: 1,
         app_version_name: electron.app.getVersion(),
@@ -20,13 +15,17 @@ function createWindow() {
         brand: os.type(),
         serial: 'foo'
     };
-    electron.session.defaultSession.webRequest.onBeforeSendHeaders(function (details, callback) {
-        details.requestHeaders['X-Molotov-Agent'] = JSON.stringify(molotovAgent);
-        details.requestHeaders.DNT = '1';
-        callback({cancel: false, requestHeaders: details.requestHeaders});
-    });
 
-    mainWindow = new electron.BrowserWindow(
+function addHeaders(details, callback) {
+    details.requestHeaders['X-Molotov-Agent'] = JSON.stringify(molotovAgent);
+    details.requestHeaders.DNT = '1';
+    callback({cancel: false, requestHeaders: details.requestHeaders});
+}
+
+function createWindow() {
+    electron.session.defaultSession.webRequest.onBeforeSendHeaders(addHeaders);
+
+    var mainWindow = new electron.BrowserWindow(
         {
             webPreferences: {
                 plugins: true
@@ -35,27 +34,12 @@ function createWindow() {
     );
 
     mainWindow.loadURL('https://app.molotov.tv/home');
-
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
 }
 
 if (electron.app) {
+    widevine.load(electron.app);
+
     electron.app.on('ready', createWindow);
-
-    electron.app.on('window-all-closed', function () {
-        if (process.platform !== 'darwin') {
-            electron.app.quit();
-        }
-    });
-
-    electron.app.on('activate', function () {
-        if (mainWindow === null) {
-            createWindow();
-        }
-    });
-
 } else {
     throw "Can't find Electron";
 }
