34ad922ac20f4f54656f69fd19103f30069b6fff
Pierre Rudloff <contact@rudloff.pro>
2018-01-04 19:01:34 +0100

    Always download latest Chrome version (#5)

--- scripts/linux-setup.sh
+++ scripts/linux-setup.sh
@@ -1,7 +1,7 @@
-DEBNAME=google-chrome-stable_61.0.3163.100-1_amd64.deb
+DEBNAME=google-chrome-stable_current_amd64.deb
 DEST=widevine
 mkdir -p $DEST
-wget https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/$DEBNAME -O $DEST/$DEBNAME
+wget https://dl.google.com/linux/direct/$DEBNAME -O $DEST/$DEBNAME
 cd $DEST
 ar x $DEBNAME
 tar -xf data.tar.xz
