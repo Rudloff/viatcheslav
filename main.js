e55ec85b88d1d79d2c61dc0a91a4b06153bd9e9b
Pierre Rudloff <contact@rudloff.pro>
2018-07-02 18:53:31 +0200

    feat: Add Content-Security-Policy header
    See https://electronjs.org/docs/tutorial/security#6-define-a-content-security-policy

--- main.js
+++ main.js
@@ -11,10 +11,29 @@ var electron = require('electron'),
  * @param {Function} callback Function called with the new headers
  * @return {Void}
  */
-function addHeaders(details, callback) {
+function addRequestHeaders(details, callback) {
     details.requestHeaders['X-Molotov-Agent'] = JSON.stringify({app_build: 3, app_id: 'electron_app'});
     details.requestHeaders.DNT = '1';
     callback({cancel: false, requestHeaders: details.requestHeaders});
+}
+
+/**
+ * Add a Content-Security-Policy header in order to mitigate XSS attacks.
+ * This also helps disabling some tracking.
+ * @param {Object}   details  Request details
+ * @param {Function} callback Function called with the new headers
+ * @return {Void}
+ */
+function addResponseHeaders(details, callback) {
+    details.responseHeaders['Content-Security-Policy'] = [
+        "default-src 'self'; object-src 'none'; " +
+                "font-src 'self' fonts.gstatic.com maxcdn.bootstrapcdn.com; " +
+                "style-src 'self' 'unsafe-inline' fonts.googleapis.com maxcdn.bootstrapcdn.com; " +
+                "connect-src fapi.molotov.tv decision.molotov.tv *.akamaized.net lic.drmtoday.com; " +
+                "img-src 'self' fusion.molotov.tv images.molotov.tv; " +
+                "media-src blob:"
+    ];
+    callback({cancel: false, responseHeaders: details.responseHeaders});
 }
 
 /**
@@ -31,7 +50,8 @@ function addCss() {
  * @return {Void}
  */
 function createWindow() {
-    electron.session.defaultSession.webRequest.onBeforeSendHeaders(addHeaders);
+    electron.session.defaultSession.webRequest.onBeforeSendHeaders(addRequestHeaders);
+    electron.session.defaultSession.webRequest.onHeadersReceived(addResponseHeaders);
 
     mainWindow = new electron.BrowserWindow(
         {
