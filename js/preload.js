6a358fd68076b68463c28af94eda2e7d238ffa1a
Pierre Rudloff <contact@rudloff.pro>
2019-06-04 23:51:58 +0200

    fix: Fix JS error
    Fixes #12

--- js/preload.js
+++ js/preload.js
@@ -1,6 +1,26 @@
 /*jslint browser: true*/
 /*global window*/
+
+/**
+ * Add HTML tag used by Molotov to inject scripts.
+ *
+ * @return {Void}
+ */
+function fixScripts() {
+    'use strict';
+    if (document.body) {
+        document.removeEventListener('DOMSubtreeModified', fixScripts);
+
+        var scripts = document.createElement('div');
+        scripts.setAttribute('id', 'scripts');
+        document.body.appendChild(scripts);
+    }
+}
+
 window.molotov = {
     app: {
     }
 };
+
+// This is a bit dirty but allows us to inject the div as soon as possible.
+document.addEventListener('DOMSubtreeModified', fixScripts, false);
